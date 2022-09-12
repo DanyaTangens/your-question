1b329fb543dab65ddd857040135000d6589401f9
Daniil <d.iluhin@k-telecom.org>
2022-09-12 09:20:57 +0500

    Content center

--- src/Auth/Component/ProtectedLayout.js
+++ src/Auth/Component/ProtectedLayout.js
@@ -1,6 +1,7 @@
 import {Navigate, useOutlet} from "react-router-dom";
 import {useAuth} from "../Hook/useAuth";
 import Navbar from "../../Navbar";
+import Container from "@mui/material/Container";
 
 export const ProtectedLayout = () => {
     const {user} = useAuth();
@@ -13,7 +14,13 @@ export const ProtectedLayout = () => {
     return (
         <div>
             <Navbar/>
-            {outlet}
-        </div>
-    );
+            <Container
+                component="main"
+                maxWidth="xm"
+                sx={{ width: 2/4 }}
+                style={{backgroundColor: "red"}}
+            >
+                {outlet}
+            </Container>
+        </div>);
 };
--- src/Navbar.js
+++ src/Navbar.js
@@ -2,14 +2,24 @@ import {Link as LinkRouter} from "react-router-dom"
 import {useAuth} from "./Auth/Hook/useAuth";
 import * as React from 'react';
 import AppBar from '@mui/material/AppBar';
-import Button from '@mui/material/Button';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import GlobalStyles from '@mui/material/GlobalStyles';
 import Link from "@mui/material/Link";
+import {IconButton, Menu, MenuItem, Tooltip} from "@mui/material";
+import Box from "@mui/material/Box";
+import Avatar from "@mui/material/Avatar";
 
 export default function Navbar() {
     const {logout} = useAuth()
+    const [anchorElUser, setAnchorElUser] = React.useState(null);
+    const handleOpenUserMenu = (event) => {
+        setAnchorElUser(event.currentTarget);
+    };
+    const handleCloseUserMenu = () => {
+        setAnchorElUser(null);
+    };
+
     return (
         <>
             <GlobalStyles styles={{ul: {margin: 0, padding: 0, listStyle: 'none'}}}/>
@@ -24,34 +34,70 @@ export default function Navbar() {
                         <LinkRouter to="/account/wall" className="site-title">YourQuestion</LinkRouter>
                     </Typography>
                     <nav>
-                        <CustomLink to="/account/wall">Главная</CustomLink>
-                        <CustomLink to="/account/message">Сообщения</CustomLink>
-                        <CustomLink to="/account/notice">Уведомления</CustomLink>
-                        <CustomLink to="/account/friends">Друзья</CustomLink>
-                        <CustomLink to="/account/profile">Профиль</CustomLink>
-                        <Button key={"logout"} onClick={logout} variant="outlined" sx={{my: 1, mx: 1.5}}>
-                            Logout
-                        </Button>
+                        <CustomLink to="/account/wall">Wall</CustomLink>
+                        <CustomLink to="/account/message">Message</CustomLink>
+                        <CustomLink to="/account/notice">Notice</CustomLink>
+                        <CustomLink to="/account/friends">Friends</CustomLink>
                     </nav>
+
+                    <Box sx={{flexGrow: 0}}>
+                        <Tooltip title={"Открыть настройки"}>
+                            <IconButton onClick={handleOpenUserMenu} sx={{p: 0}}>
+                                <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg"/>
+                            </IconButton>
+                        </Tooltip>
+                        <Menu
+                            sx={{mt: '45px'}}
+                            id="menu-appbar"
+                            anchorEl={anchorElUser}
+                            anchorOrigin={{
+                                vertical: 'top', horizontal: 'right',
+                            }}
+                            keepMounted
+                            transformOrigin={{
+                                vertical: 'top', horizontal: 'right',
+                            }}
+                            open={Boolean(anchorElUser)}
+                            onClose={handleCloseUserMenu}
+                        >
+                            <MenuItem key={"profile"} onClick={handleCloseUserMenu}>
+                                <Typography textAlign="center">
+                                    <CustomLink to="/account/profile">Profile</CustomLink>
+                                </Typography>
+                            </MenuItem>
+                            <MenuItem key={"setting"} onClick={handleCloseUserMenu}>
+                                <Typography textAlign="center">
+                                    <CustomLink to="/account/profile">
+                                        Profile
+                                    </CustomLink>
+                                </Typography>
+                            </MenuItem>
+                            <MenuItem key={"logout"} onClick={logout}>
+                                <Typography textAlign="center">
+                                    <CustomLink to="/account/logout">
+                                        LogOut
+                                    </CustomLink>
+                                </Typography>
+                            </MenuItem>
+                        </Menu>
+                    </Box>
                 </Toolbar>
             </AppBar>
-        </>
-    )
+        </>)
 }
 
 function CustomLink({to, children, ...props}) {
-    return (
-        <>
-            <Link
-                variant="button"
-                color="text.primary"
-                href="#"
-                sx={{ my: 1, mx: 1.5 }}
-            >
-                <LinkRouter to={to} {...props}>
-                    {children}
-                </LinkRouter>
-            </Link>
-        </>
-    )
+    return (<>
+        <Link
+            component="span"
+            variant="button"
+            color="text.primary"
+            style={{textDecoration: 'none'}}
+            sx={{my: 1, mx: 1.5}}
+        >
+            <LinkRouter to={to} {...props}>
+                {children}
+            </LinkRouter>
+        </Link>
+    </>)
 }
